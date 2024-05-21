8b26a0ad05d9d4f0ec3eb9d5ca2a6779e54b7bc3
Whaeedil <baoppyhossen1234@gmail.com>
2024-05-22 03:44:34 +0600

    2nd commit

--- src/assets/components/shared/Navbar.jsx
+++ src/assets/components/shared/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
     }
     return (
         <div>
+
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
