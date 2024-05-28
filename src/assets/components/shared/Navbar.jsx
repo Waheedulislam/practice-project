b9c26f637fd1c86d631048bc94f0319c57344792
Whaeedil <baoppyhossen1234@gmail.com>
2024-05-29 03:04:11 +0600

    final

--- src/assets/components/shared/Navbar.jsx
+++ src/assets/components/shared/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">King</a>
+                    <a className="btn btn-ghost text-xl">King Restaurant</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
